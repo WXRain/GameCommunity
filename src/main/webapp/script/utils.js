b81095ef508f60d133c199f7f934e113eb441c95
WXRain <wangxinyu@wangxinyudeMacBook-Pro.local>
2017-05-11 11:12:36 +0800

    Bootstrap for game/main.html

--- src/main/webapp/script/utils.js
+++ src/main/webapp/script/utils.js
@@ -1,4 +1,4 @@
-	
+
 function createPage(pagingData, tdNum){
 	if(pagingData.totalPage == 0) pagingData.totalPage = 1;
 	if(pagingData.totalNum == 0){
@@ -8,15 +8,15 @@ function createPage(pagingData, tdNum){
 	for(var i = 0; i < tdNum - 2; i++){
 		$("#table").append("<td></td>");
 	}
-	$("#table").append("<td>第" + pagingData.currentPage + "页，共" + pagingData.totalPage + "页，共" + pagingData.totalNum + "条记录</td><td id='paging'></td></tr>");
-	$("#paging").append("<a href=javascript:void() onclick=jumpToPage(" + 1 + ")> 首页 </a>");
+	$("#table").append("<td class='text-label'>第" + pagingData.currentPage + "页，共" + pagingData.totalPage + "页，共" + pagingData.totalNum + "条记录</td><td id='paging'></td></tr>");
+	$("#paging").append("<a class='a-label' href=javascript:void() onclick=jumpToPage(" + 1 + ")> 首页 </a>");
 	if(pagingData.currentPage != 1){
-		$("#paging").append("<a href=javascript:void() onclick=jumpToPage(" + (pagingData.currentPage - 1) + ")> 上一页 </a>");
+		$("#paging").append("<a class='a-label' href=javascript:void() onclick=jumpToPage(" + (pagingData.currentPage - 1) + ")> 上一页 </a>");
 	}
 	if(pagingData.currentPage != pagingData.totalPage){
-		$("#paging").append("<a href=javascript:void() onclick=jumpToPage(" + (pagingData.currentPage + 1) + ")> 下一页 </a>");
+		$("#paging").append("<a class='a-label' href=javascript:void() onclick=jumpToPage(" + (pagingData.currentPage + 1) + ")> 下一页 </a>");
 	}
-	$("#paging").append("<a href=javascript:void() onclick=jumpToPage(" + (pagingData.totalPage) + ")> 末页 </a>");
+	$("#paging").append("<a class='a-label' href=javascript:void() onclick=jumpToPage(" + (pagingData.totalPage) + ")> 末页 </a>");
 }
 
 function showUser(userId){
